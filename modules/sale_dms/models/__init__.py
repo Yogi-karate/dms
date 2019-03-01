c26433772fc647523fc6b69e757fa1f28e0847f2
yogi-karate <simha.karate@gmail.com>
2019-03-01 08:40:00 +0000

    new branch for 1.1 version

--- modules/sale_dms/models/__init__.py
+++ modules/sale_dms/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 from . import product
 from . import product_pricelist
 from . import sales_team
