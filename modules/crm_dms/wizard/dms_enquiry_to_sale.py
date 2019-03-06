d46183788d51a691e624c863765f39c45b169442
yogi-karate <simha.karate@gmail.com>
2019-03-06 11:39:12 +0000

    Changes to Auto Sale order

--- modules/crm_dms/wizard/dms_enquiry_to_sale.py
+++ modules/crm_dms/wizard/dms_enquiry_to_sale.py
@@ -7,7 +7,6 @@ from odoo.tools.translate import _
 
 
 class Lead2OpportunityPartner(models.TransientModel):
-
     _name = 'dms.enquiry2sale.order'
     _description = 'Create Quote for Opportunity (not in mass)'
 
@@ -42,7 +41,7 @@ class Lead2OpportunityPartner(models.TransientModel):
             if enquiry.product_variant:
                 result['product_variant'] = enquiry.product_variant.id
             if enquiry.product_id:
-                 result['product_id'] = enquiry.product_id.id
+                result['product_id'] = enquiry.product_id.id
             if not partner_id and not lead.contact_name:
                 result['action'] = 'nothing'
         return result
@@ -63,6 +62,7 @@ class Lead2OpportunityPartner(models.TransientModel):
     product_id = fields.Many2one('product.template', string='Product', required=True)
     product_color = fields.Many2one('product.template.attribute.value', string='Color')
     product_variant = fields.Many2one('product.template.attribute.value', string='Variant')
+    pricelist = fields.Many2one('product.pricelist', string='Pricelist')
 
     @api.onchange('action')
     def onchange_action(self):
@@ -78,7 +78,9 @@ class Lead2OpportunityPartner(models.TransientModel):
         """
         if self.user_id:
             if self.team_id:
-                user_in_team = self.env['crm.team'].search_count([('id', '=', self.team_id.id), '|', ('user_id', '=', self.user_id.id), ('member_ids', '=', self.user_id.id)])
+                user_in_team = self.env['crm.team'].search_count(
+                    [('id', '=', self.team_id.id), '|', ('user_id', '=', self.user_id.id),
+                     ('member_ids', '=', self.user_id.id)])
             else:
                 user_in_team = False
             if not user_in_team:
@@ -110,7 +112,7 @@ class Lead2OpportunityPartner(models.TransientModel):
             self_def_user = self.with_context(default_user_id=self.user_id.id)
             partner_id = \
                 self_def_user._create_partner(
-                lead.id, self.action, vals.get('partner_id') or lead.partner_id.id)
+                    lead.id, self.action, vals.get('partner_id') or lead.partner_id.id)
             res = lead.convert_opportunity(partner_id, [], False)
         user_ids = vals.get('user_ids')
 
@@ -130,44 +132,68 @@ class Lead2OpportunityPartner(models.TransientModel):
         """
         self.ensure_one()
         sale = self.env['sale.order']
-        product = self.env['product.product'].search([('product_tmpl_id','=',self.product_id.id),
-                                                      ('color_value','=',self.product_color.name),
-                                                      ('variant_value','=',self.product_variant.name)], limit=1)
+        product = self.env['product.product'].search([('product_tmpl_id', '=', self.product_id.id),
+                                                      ('color_value', '=', self.product_color.name),
+                                                      ('variant_value', '=', self.product_variant.name)], limit=1)
+        print("************************************************************************")
+        print(self.pricelist)
         if not product:
             raise UserError(_("Unable to create Quote as product not found"))
-
         values = {
             'team_id': self.team_id.id,
             'partner_id': self.partner_id.id,
-            'user_id':self.user_id.id,
+            'user_id': self.user_id.id,
             'opportunity_id': self._context['active_id']
         }
+
         if self.partner_id:
             values['partner_id'] = self.partner_id.id
         order = sale.create(values)
-        self._create_sale_order_line(product,order)
 
-    def _create_sale_order_line(self,product,order):
-          order_line = self.env['sale.order.line']
-          vals = {
-              'product_id':product.id,
-              'name': product.name,
-              'order_id':order.id
-          }
-          order_line.create(vals)
+        self._create_sale_order_line(product, self.pricelist, order)
+
+    def _create_sale_order_line(self, product, component, order):
+        order_line = self.env['sale.order.line']
+        vals = {
+            'product_id': product.id,
+            'name': product.name,
+            'order_id': order.id
+        }
+        for itemlist in component.item_ids:
+            print("#####################", itemlist)
+            for item in itemlist:
+                for compos in item.component:
+                    product = self.env['product.product'].search([('name', '=', compos.type_id.name)])
+                    if not product:
+                        product = self._prepare_component_product(compos.type_id.name)
+                    vals = {
+                        'product_id': product.id,
+                        'name': compos.type_id.name,
+                        'price_unit': compos.price,
+                        'order_id': order.id
+                    }
+            print(vals)
+            order_line.create(vals)
+
+            print(item)
+        order_line.create(vals)
+
+    def _prepare_component_product(self, component_name):
+        return {
+            'name': component_name,
+            'type': 'service',
+            'company_id': False,
+        }
 
     def _create_partner(self, lead_id, action, partner_id):
         """ Create partner based on action.
             :return dict: dictionary organized as followed: {lead_id: partner_assigned_id}
         """
-        #TODO this method in only called by Lead2OpportunityPartner
-        #wizard and would probably diserve to be refactored or at least
-        #moved to a better place
+        # TODO this method in only called by Lead2OpportunityPartner
+        # wizard and would probably diserve to be refactored or at least
+        # moved to a better place
         if action == 'each_exist_or_create':
             partner_id = self.with_context(active_id=lead_id)._find_matching_partner()
             action = 'create'
         result = self.env['crm.lead'].browse(lead_id).handle_partner_assignation(action, partner_id)
         return result.get(lead_id)
-
-
-
