a269b5d5b361d7aaa792c41adea3dcf41a46597a
tramm <ram.tangirala@gmail.com>
2019-03-08 18:26:30 +0530

    pricelist added to sale order auto create values

--- modules/crm_dms/wizard/dms_enquiry_to_sale.py
+++ modules/crm_dms/wizard/dms_enquiry_to_sale.py
@@ -159,7 +159,7 @@ class Lead2OpportunityPartner(models.TransientModel):
         vals = {
             'product_id': product.id,
             'name': product.name,
-            'order_id': order.id
+            'order_id': order.id,
             'pricelist_id':self.pricelist_id.id
         }
         order_line.create(vals)
