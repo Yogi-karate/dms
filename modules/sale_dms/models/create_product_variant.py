5dd97873c7cd50d8f588275191e930f0fe574cf0
Yogi-karate <simha.karate@gmail.com>
2019-04-16 14:17:03 +0530

    changes of attribute vallues, adding template attribute line to template on create product variant screen.

--- modules/sale_dms/models/create_product_variant.py
+++ modules/sale_dms/models/create_product_variant.py
@@ -10,49 +10,91 @@ class DmsProduct(models.TransientModel):
     product_color = fields.Many2one('product.attribute.value', string='Color')
     product_variant = fields.Many2one('product.attribute.value', string='Variant')
     show_color = fields.Boolean('Color Visible', default=False)
+    variant_attribute_values = fields.One2many('product.attribute.value', string='attributes',
+                                               compute='compute_variant_attribute_values')
     color_attribute_values = fields.One2many('product.attribute.value', string='attributes',
                                              compute='compute_color_attribute_values')
     name = fields.Char(string='name',compute='_compute_name')
 
     @api.model
     def create(self, vals):
+        print(vals['product_color'])
+        for x in vals:
+            print(x)
         # context: no_log, because subtype already handle this
         res = super(DmsProduct, self).create(vals)
         res.create_product_variant()
         return res
 
+
+
+    @api.onchange('product_id')
+    def compute_variant_attribute_values(self):
+        if self.variant_attribute_values or self.color_attribute_values:
+            self.product_variant = False
+            self.product_color = False
+        self.variant_attribute_values = None
+        self.color_attribute_values = None
+        products = self.sudo().env['product.template'].search([('id', '=', self.product_id.id)])
+        self.variant_attribute_values = products.mapped('valid_product_attribute_value_ids').filtered(
+            lambda attrib: attrib.attribute_id.name.lower() == 'variant')
+        print(self.variant_attribute_values)
+
     @api.onchange('product_variant')
     def compute_color_attribute_values(self):
-        products = self.sudo().env['product.product'].search(
-            [('product_tmpl_id', '=', self.product_id.id), ('variant_value', '=', self.product_variant.name)])
+        if self.color_attribute_values:
+            self.product_color = False
+            self.color_attribute_values = None
+        products = self.sudo().env['product.template'].search(
+            [('id', '=', self.product_id.id)])
         for x in products:
-            print(x.id, "_____" + x.name + "_____" + x.variant_value + "___" + x.color_value)
-        self.color_attribute_values = products.mapped('attribute_value_ids')
+            print("^^^^^^^^^^^^^",x.valid_product_attribute_value_ids)
+        self.color_attribute_values = products.mapped('valid_product_attribute_value_ids')
         self.show_color = True
-        print(self.color_attribute_values)
+        print(self.color_attribute_values,"***************************************")
+
     @api.one
     @api.depends('product_id')
     def _compute_name(self):
-        self.name = self.product_id.name +"(" +self.product_color.name +","+ self.product_variant.name+")"
+        color = self.env['product.attribute.value'].search([('id', '=', self.product_variant.id)])
+        variant = self.env['product.attribute.value'].search([('id', '=', self.product_color.id)])
+        if color and variant:
+             self.name = self.product_id.name + color.name + variant.name
+             print(color.name,"############################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",variant.name)
+
+
 
 
 
     @api.multi
     def create_product_variant(self):
-        product = self.env['product.product']
-        vals = {
-            'name': self.product_id.name,
-            'company_id': False,
-            'product_tmpl_id': self.product_id.id,
-            'attribute_value_ids': [(6, '_', [self.product_color.id, self.product_variant.id])]
-        }
-        exiting_product= self.sudo().env['product.product'].search(
-            [('product_tmpl_id', '=', self.product_id.id), ('variant_value', '=', self.product_variant.name) ,('color_value','=',self.product_color.name)])
-        if not exiting_product:
-                 product.create(vals)
+        template = self.env['product.template'].browse(self.product_id.id)
+        existing_product = self.env['product.product'].search([('product_tmpl_id', '=', self.product_id.id),
+                                                      ('color_value', '=', self.product_color.name),
+                                                      ('variant_value', '=', self.product_variant.name)], limit=1)
+        if existing_product:
+            raise UserError(_("Product already exists"))
         else:
-            raise UserError(_("Unable to Create as product already exists"))
-        print("***************************************************************88")
-        print(self.product_id.name)
-        print(self.product_color.id)
-        print(self.product_variant.id)
+            attribute_line = self.env['product.template.attribute.line'].search([('product_tmpl_id','=',self.product_id.id)])
+            if attribute_line :
+                attribute_line [0].value_ids += self.product_color
+                attribute_line [1].value_ids += self.product_variant
+                template_values= {
+                    'name': self.product_id.name,
+                    'attribute_line_ids': [(6, '_', [attribute_line [0].id, attribute_line [1].id])]
+                }
+            else:
+                raise UserError(_("There are no Template attributes. Please go back to Product screen and edit ",
+                                  "You are not allowed create Template attributes here"))
+
+            template.write(template_values)
+            product = self.env['product.product']
+            vals = {
+                'name': template.name,
+                'company_id': False,
+                'product_tmpl_id': self.product_id.id,
+                'attribute_value_ids'
+                '': [(6, '_', [self.product_color.id, self.product_variant.id])],
+                'display_name': self.name
+            }
+            product.create(vals)
