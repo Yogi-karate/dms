6ed4c1528ecf54e403f1cbbff5d703eb2270e207
Yogi-karate <simha.karate@gmail.com>
2019-05-30 16:45:31 +0530

    first commit for neo"

---
+++ neo/dms/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'dms',
+    'version': '1.0',
+    'category': 'Sale',
+    'sequence': 60,
+    'summary': 'Handle Sales customization for DMS',
+    'description': "Vehicle Dealership Business Domain",
+    'depends': ['base_dms','purchase','crm_dms','vehicle','l10n_in'],
+    'data': [
+        'views/dms_views.xml',
+        'security/dms_groups.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
