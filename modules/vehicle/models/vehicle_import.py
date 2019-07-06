bf6157530661855ce9fda9511ae4e741f388acfd
tramm <ram.tangirala@gmail.com>
2019-07-06 18:05:11 +0530

    update to vehicle import job

--- modules/vehicle/models/vehicle_import.py
+++ modules/vehicle/models/vehicle_import.py
@@ -1,7 +1,10 @@
+import logging
 from odoo import api, fields, models, tools
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 from datetime import datetime
+
+_logger = logging.getLogger(__name__)
 
 
 class ODVehicle(models.TransientModel):
@@ -23,17 +26,22 @@ class ODVehicle(models.TransientModel):
     chassis_no = fields.Char('Chassis No')
 
     def create_vehicles(self):
+        _logger.info("!!!!!!!!!!!!!!Starting Creation of Vehicle from Import Data!!!!!!!!!!!!!!!!")
         self._create_vehicles()
         self.env['dms.vehicle.import'].search([('status', '=', True)]).unlink()
+        _logger.info("!!!!!!!!!!!!!!End of ->  Creation of Vehicle from Import Data!!!!!!!!!!!!!!!!")
 
     def _create_vehicles(self):
         od_vehicles = self.env['dms.vehicle.import'].search([], limit=1000)
+        _logger.info("The number of records to process =>",len(od_vehicles))
         for vehicle in od_vehicles:
             self = self.sudo()
             product = self.env['product.product'].search(
                 [('name', 'ilike', vehicle.model), ('fuel_type', 'ilike', vehicle.fuel_type)], limit=1)
-            if not product or vehicle.name or vehicle.customer_name or vehicle.date_of_sale:
+            print("In Vehicle loop of import ^^^^^^^^",vehicle.customer_name,vehicle.vin_no,vehicle.date_of_sale)
+            if not product or not vehicle.vin_no or not vehicle.customer_name or not vehicle.date_of_sale:
                 continue
+            print("-----------Starting creation of partner and vehicle------------")
             partner = self.env['res.partner'].create(vehicle.create_partner(vehicle))
             vals = {
                 'name': vehicle.vin_no,
@@ -59,7 +67,6 @@ class ODVehicle(models.TransientModel):
         od_vehicles = self.env['vehicle'].search([('ref', '=', False)])
         for vehicle in od_vehicles:
             vehicle.source = 'od'
-
 
     def create_partner(self, vehicle):
         return {
